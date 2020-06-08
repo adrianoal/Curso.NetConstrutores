2d22de609f77f24c7e951fc266169b0f2269b229
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-06-08 17:13:06 -0300

    Exemplo com construtor

---
+++ Construtures/Testes/Produto.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+
+namespace EstoqueProdutosAula42
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+
+        // P/ fazer uma sobreposicao, tem q escrever a palavra "override"
+        public override string ToString()
+        {
+            return Nome
+                + ", R$ "
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: R$ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+
+    }
+}
---
+++ Construtures/Testes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstoqueProdutosAula42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Entre com os dados do Produto: ");
+            Console.WriteLine(String.Empty);
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no Estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine(string.Empty);
+
+            Produto p = new Produto(nome, preco, quantidade);
+           
+            Console.WriteLine("Dados do Produto: " + p);
+            Console.WriteLine(string.Empty);// Quebra de linha
+
+            // ADICIONADO NO ESTOQUE ESTOQUE
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtde);
+
+            Console.WriteLine(); // Quebra de linha 
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+            // ADICIONADO NO REMOVIDO DO ESTOQUE
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtde);
+
+            Console.WriteLine(); // Quebra de linha 
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+
+        }
+    }
+}
